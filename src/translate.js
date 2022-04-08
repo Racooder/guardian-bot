fb1ab7a813073833072a76955f4667499e53b6f6
Racooder <76227605+Racooder@users.noreply.github.com>
2022-04-08 15:40:58 +0200

    1.0.4 Supplement
    Someday it will work the first time.

---
+++ src/translate.js
@@ -0,0 +1,30 @@
+const { existsSync } = require('fs');
+
+/**
+ * Translates the given text into another
+ * @param {String} key - The text key
+ * @param {String} dictionary - The dictionary key
+ * @returns {String} The text in the given language.
+ */
+module.exports = (key, dictionary) => {
+    try {
+        if (existsSync(`./src/dictionaries/${dictionary}.json`)) {
+            let translation = require(`./dictionaries/${dictionary}`)[key];
+
+            if (translation == null || translation == ""){
+                translation = require('./dictionaries/game/en.json')[key];
+            }
+
+            if (translation == null || translation == ""){
+                translation = "This text does not exist. (Please report this to the developer)";
+            }
+
+            return translation;
+        }
+        else{
+            return null;
+        }
+    } catch(err) {
+        console.error(err)
+    }
+}
