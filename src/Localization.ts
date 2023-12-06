968ce01fe137291423d5fd85f4342fe1aa800341
Racooder <76227605+Racooder@users.noreply.github.com>
2023-12-06 23:49:10 +0100

    ✨ Added localization

---
+++ src/Localization.ts
@@ -0,0 +1,37 @@
+import { existsSync } from "fs";
+
+const LOCALIZATION_FOLDER = "localization";
+const FALLBACK_LANGUAGE = 'en';
+
+var localization: { [key: string]: { [key: string]: string } } = {};
+
+function loadLanguage(language: string): void {
+    if (localization[language]) {
+        return;
+    }
+    if (!existsSync(`${LOCALIZATION_FOLDER}/${language}.json`)) {
+        return;
+    }
+    localization[language] = require(`../${LOCALIZATION_FOLDER}/${language}.json`);
+}
+
+/**
+ * Returns
+ * @param key - The key to localize
+ * @param language - ISO 639-1 standard language codes
+ * @returns The text in the specified language, or the fallback language if the key is not found
+ */
+export function localize(key: string, language: string): string {
+    loadLanguage(language);
+    if (localization[language]) {
+        if (localization[language][key]) {
+            return localization[language][key];
+        }
+    }
+    if (localization[FALLBACK_LANGUAGE]) {
+        if (localization[FALLBACK_LANGUAGE][key]) {
+            return localization[FALLBACK_LANGUAGE][key];
+        }
+    }
+    return key;
+}
